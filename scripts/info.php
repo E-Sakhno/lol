1a0ff9a5a876d181e6aba9a1bfe6f1b22c7d3479
E-Sakhno <miss.saxno@yandex.ru>
2022-05-12 03:18:58 +0300

    добавлены ранговая информация

--- scripts/info.php
+++ scripts/info.php
@@ -4,9 +4,21 @@ if ($_GET['region'] == 'all') {
     $ru = json_decode(file_get_contents('json/ru_summoners_arr.json'), true);
     $euw1 = json_decode(file_get_contents('json/euw1_summoners_arr.json'), true);
     $info = array_merge($ru, $euw1);
-} else {
+    
+    
+    $ru_rang = json_decode(file_get_contents('json/ru_summoners_rang.json'), true);
+    $euw1_rang = json_decode(file_get_contents('json/euw1_summoners_rang.json'), true);
+    $info_rang = array_merge($ru, $euw1);
+} 
+
+else {
     $info = json_decode(
         file_get_contents('json/'.$_GET['region'] . '_summoners_arr.json'),
+        true
+    );
+
+    $info_rang = json_decode(
+        file_get_contents('json/'.$_GET['region'] . '_summoners_rang.json'),
         true
     );
 }
