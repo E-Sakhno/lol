9a83ed632b68ffa0747c3519433ec0564c4419ea
E-Sakhno <miss.saxno@yandex.ru>
2022-05-14 02:00:30 +0300

    добавлена многостраничность

--- record_total.php
+++ record_total.php
@@ -1,25 +1,20 @@
-<p>
-    Топ по очкам на героях в сумме
+<?php  
+include 'header.php';
+
+echo '<h1>'.$lang['Top-h'] . '</h1>';
+?>
 
     <form action="record_total.php" method="get" name="form">
-    <?php include_once 'scripts/region.php'; ?>
-    <br>
-    <?php include_once 'scripts/queue.php'; ?>
-
-    <br>
-    Количество записей: 
-    <?php include_once 'scripts/amount.php'; ?>
-<br>
-
-<br>    
+    <?php 
+    include_once 'scripts/region.php';
+    include_once 'scripts/queue.php'; 
+    include_once 'scripts/amount.php'; ?>
     <!-- <input type="submit"> -->
-    <button class="btn btn-success btn">Кнопка</button>
+    <button class="btn btn-success btn">&#128269;</button>
 </form>
 
-</p>
 
 <?php
-include 'header.php';
 if (isset($_GET['region'])) {
     include 'scripts/info.php';
     // print_r($info_rang);
@@ -28,9 +23,23 @@ if (isset($_GET['region'])) {
         $summoners[$key] = $row[$k['total']];
     }
     array_multisort($summoners, SORT_DESC, $info);
+    $counter = 0;
+    $sum_by_num = [];
+    foreach ($summoners as $key => $value){
+        $sum_by_num[$counter] = $key;
+        $counter++;
+
+    }
+    // // print_r  (array_count_values($sum_by_num)) ;
+    // echo '<B>' .  count($sum_by_num) . ' </B>';
+    // echo '<B>' .  count($summoners) . ' </B>';
+    // echo '<B>' .  count($info) . ' </B>';
     // echo "<BR>";
     // echo "<BR>";
     // print_r ($summoners);
+    echo "<BR>";
+    echo "<BR>";
+    // print_r($sum_by_num);
 
     $champs_name = json_decode(
             file_get_contents('json/'. $_COOKIE['lang'] . '_champs_name.json'),
@@ -44,18 +53,29 @@ if (isset($_GET['region'])) {
     <tr>
     <th data-type="number"> № </th>
     <th> Ник </th>          
-    <th> Регион </th>         
     <th data-type="number"> Лвл </th>
     <th data-type="number"> Очки </th>
     <th> Ранг </th></tr>
     </thead>
         <tbody>'
     ;
+    
+    if (isset($_GET['p'])){
 
-    $ctr = 1;
+        $page = $_GET['p'];
+    }
+    else{
+        $page = 1;
+    }
+
+    $ctr = 1 +  $_GET['amount'] * ($page-1);
+    // echo $page;
     // print_r($info[$key]);
-    foreach ($summoners as $key => $value) {
-        if (array_key_exists($key, $info_rang)){
+    for ($ctr; $ctr <= $_GET['amount']*$page; $ctr++){
+
+        $key = $sum_by_num[$ctr-1];
+        if (array_key_exists($sum_by_num[$ctr-1], $info_rang)){
+            
             $img =  $add[$info_rang[$key][$l['tier']]] .  '<img src="img/Emblem_' . $info_rang[$key][$l['tier']] . ".png\">";
             if ($info_rang[$key][$l['tier']] == "CHALLENGER" and $info_rang[$key][$l['rank']] == 'I'){
                 $elo = $info_rang[$key][$l['tier']] . ' ' . $info_rang[$key][$l['lp']];
@@ -66,36 +86,63 @@ if (isset($_GET['region'])) {
             }
         }
         else{
-            $img = '';
-            $elo = '&zwnj;&zwnj; - ';
+            $img = '&zwnj;&zwnj;';
+            $elo = '';
             
         }
     
         echo '<tr><td>' . $ctr . 
-        "</td><td>
+        "</td><td><div class=\"summoner\">
         <img src=\"http://ddragon.leagueoflegends.com/cdn/12.8.1/img/profileicon/" . $info[$key][$k['icon']] . '.png">
         <a href="full_info.php?nick=' . $info[$key][$k['nick']] . "&region="  . $info[$key][$k['region']] . "\">" .
-            $info[$key][$k['nick']] .
-            '</a></td><td>' .
+        '<div class="nick">'  .  
+        $info[$key][$k['nick']] .
+            '</a><div class="region"> ' .
             $info[$key][$k['region']] .
-            '</td><td>'.
+            '</div></div></td><td>'.
+            
             $info[$key][$k['lvl']].
             '</td><td>' . 
-            number_format($value, 0, ',', ' ') .
+            number_format($summoners[$key], 0, ',', ' ') .
             // $value .
-            '</td><td>' . 
+            '</td><td>' . '<div class="rank">' .
             $img .
-            $elo .   
+            '<div class="elo">' .  $elo .   
+            '</div></div>' 
+            .
             '</td></tr>';
             
-            $ctr++;
         if (isset($_GET['amount'])) {
-            if ($ctr > $_GET['amount']) {
+            if ($ctr == count($sum_by_num)) {
                 break;
             }
         }
     }
     echo '</tbody></table>';
+    
+    $urlParams = '&region=' . $_GET['region'] . '&qu=' . $_GET['qu'] . '&amount=' . $_GET['amount'] . '&p=';
+    echo '<div class="pages">';
+    if ($page > 1){
+
+        echo '<div class="pagenum"><a href="record_total.php?'. $urlParams . $page-1 . '"> < </a></div>';
+    }
+    for ($n=1; $n <= ceil(count($sum_by_num)/$_GET['amount']); $n++){
+        $url = $urlParams . $n;
+        if ($n == $page){
+            $cur = 'current';
+        }
+        else{
+            $cur = '';
+        }
+        echo '<div class="pagenum ' .$cur. '"><a href="record_total.php?'. $url . '">' . $n . " </a></div>";
+    }
+    if ($page < ceil(count($sum_by_num)/$_GET['amount'])){
+
+        echo '<div class="pagenum"><a href="record_total.php?'. $urlParams . $page+1 . '"> > </a></div>';
+    }
+    echo "</div>";
+    // echo ceil(count($sum_by_num)/$_GET['amount']);
+
 }
 
 
