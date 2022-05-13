eaa1d882be21b11962d05a216ea6f66ecbbfefad
E-Sakhno <miss.saxno@yandex.ru>
2022-05-14 02:00:46 +0300

    изменены значения

--- scripts/amount.php
+++ scripts/amount.php
@@ -1,12 +1,13 @@
+<?php echo $lang['Post']; ?>
 <select name="amount">
-<option value="1" 
-    <?php if (isset($_GET['amount'])){if ($_GET['amount'] == "1"){echo 'selected';}} ?>
-> 1
-<option value="5" 
-    <?php if (isset($_GET['amount'])){if ($_GET['amount'] == "5"){echo 'selected';}} ?>
-> 5
 <option value="10" 
     <?php if (isset($_GET['amount'])){if ($_GET['amount'] == "10"){echo 'selected';}} ?>
 > 10
+<option value="50" 
+    <?php if (isset($_GET['amount'])){if ($_GET['amount'] == "50"){echo 'selected';}} ?>
+> 50
+<option value="100" 
+    <?php if (isset($_GET['amount'])){if ($_GET['amount'] == "100"){echo 'selected';}} ?>
+> 100
 
 </select>
