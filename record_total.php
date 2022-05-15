12887f094ede37f1e62a1bc96f6c898d2097f2c0
E-Sakhno <miss.saxno@yandex.ru>
2022-05-16 02:31:56 +0300

    Сделана форма поиска по нику места человека

--- record_total.php
+++ record_total.php
@@ -11,12 +11,58 @@ echo '<h1>'.$lang['Top-h'] . '</h1>';
     include_once 'scripts/amount.php'; ?>
     <!-- <input type="submit"> -->
     <button class="btn btn-success btn">&#128269;</button>
+    <br>
+<form action="record_total.php?" method="get" name="form">
+Nick: <input name="nick" type="text" class="form-control inp" value="" placeholder="">
+<?php include 'scripts/region_search.php'; ?>
+<button class="btn btn-success btn">&#128269;</button>
+</form>
 </form>
 
 
+
+
 <?php
+include 'api.php';
 if (isset($_GET['region'])) {
     include 'scripts/info.php';
+    if (isset($_GET['nick']) and $_GET['nick'] != ''){
+        $nick = $_GET['nick'];
+        $nick_repl = str_replace(' ', '%20', $nick);
+        $region_s = $_GET['region_s'];
+        $summoner_info = json_decode(
+            file_get_contents(
+                'https://' .
+                    $region_s .
+                    '.api.riotgames.com/lol/summoner/v4/summoners/by-name/' .
+                    $nick_repl .
+                    '?api_key=' .
+                    $api
+            ),
+            true
+        );
+        // print_r ($summoner_info);
+        
+        if ($summoner_info == NULL){
+            echo "Такого пользователя не найдено!";
+        }
+        else{
+        global $summoner_id;
+        $summoner_id = $summoner_info['id'];
+                
+        $sum_num = array_key_exists($summoner_id, $info);
+        // echo $summoner_id;
+        // print_r ($info);
+        // echo $sum_num;
+        if ($sum_num == 0){
+            // echo 'не нашли';
+            include "scripts/sum_info.php";
+         }}
+         unset($info);
+    include 'scripts/info.php';
+
+
+
     // print_r($info_rang);
     $summoners = [];
     foreach ($info as $key => $row) {
@@ -30,6 +76,8 @@ if (isset($_GET['region'])) {
         $counter++;
 
     }
+    $sum_num = array_search($summoner_id, $sum_by_num);
+
     // // print_r  (array_count_values($sum_by_num)) ;
     // echo '<B>' .  count($sum_by_num) . ' </B>';
     // echo '<B>' .  count($summoners) . ' </B>';
@@ -48,7 +96,14 @@ if (isset($_GET['region'])) {
         );
     // print_r($champs_name);
 
-   
+//    echo "KEY: ";
+//    (print_r($sum_num));
+    //    echo $sum_num;
+        if ($sum_num !=0){
+        echo "Призыватель " . $summoner_info['name'] . ': ' .  $sum_num+1;
+        }
+}
+
 
     $amount = ceil(str_replace(',', '.', $_GET['amount']));
 
@@ -60,10 +115,11 @@ if (isset($_GET['region'])) {
         $page = 1;
     }
 
+    
     $ctr = 1 +  $amount * ($page-1);
     // echo $page;
     // print_r($info[$key]);
-    $urlParams = '&region=' . $_GET['region'] . '&qu=' . $_GET['qu'] . '&amount=' . $amount . '&p=';
+    $urlParams = '&region=' . $_GET['region'] . '&qu=' . $_GET['qu'] . '&amount=' . $amount . '&nick=' . $nick . "&region_s=" . $region_s. '&p=';
     $page_last = ceil(count($sum_by_num)/$amount);
 
     if ($page_last <=6){
