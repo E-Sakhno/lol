1164091410a977c664c85d55b4ebcdbb32acbb4f
E-Sakhno <miss.saxno@yandex.ru>
2022-05-20 19:01:09 +0300

    вынесен в отдельный файл скрипт блокировки кнопки поиска

---
+++ scripts/block.js
@@ -0,0 +1,14 @@
+let inp = document.querySelector('input.inp');
+let sel = document.querySelector('select.inp');
+let btn = document.querySelector('.btn');
+btn.setAttribute('disabled', true);
+ 
+inp.oninput = function(){
+let val = inp.value;
+  if (val.length < 1){
+    btn.setAttribute('disabled', true);
+  }else{
+    btn.removeAttribute('disabled');
+  }
+
+}
