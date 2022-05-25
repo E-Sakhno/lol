a90f3f7382af50b33abbe5044e296d62f04dcd45
E-Sakhno <miss.saxno@yandex.ru>
2022-05-25 03:05:33 +0300

    добавлены страницы регенов

---
+++ champs_regenMP.php
@@ -0,0 +1,42 @@
+<?php
+include "header.php";
+echo '<h1>' . $lang['Regen'] . ' ' . $lang['MPs'] . '</h1>';
+?>
+<title><?php echo $lang['Regen'];?></title>
+<div class="blok"> 
+        <table id="sortable" border="1" >     
+        
+        <thead>
+        <tr>
+    <th><?php echo $lang['Hero'];?></th>       
+    <th data-type="number"><?php echo $lang['Default'];?></th>          
+    <th data-type="number"><?php echo $lang['perlvl'];?></th>          
+    <th data-type="number"><?php echo $lang['at18'];?></th>          
+    </tr>
+    </thead>
+    <tbody>
+
+
+
+<?php
+include 'api.php';
+
+$champsInfo = json_decode(file_get_contents('json/'.$_COOKIE['lang'].'_champs.json'), true);
+
+
+foreach ($champsInfo['data'] as $key => $value){
+echo "
+<tr>
+<td id=\"" . $value['key'] . "\"><div class=\"fullchamp\"><img id=\"" . $value['name']. "\" src=\"http://ddragon.leagueoflegends.com/cdn/". $version . "/img/champion/" . $value['image']['full'] . "\">" .
+'<div class="fullchampname">' . $value['name'] .'</div></div></td>' .
+'<td class="center">' . $value['stats']['mpregen']/5 . '</td>' . 
+'<td class="center">' . $value['stats']['mpregenperlevel']/5 . '</td>' . 
+'<td class="center">' . $value['stats']['mpregen']/5 +  17 * $value['stats']['mpregenperlevel']/5 . '</td>' . 
+'</tr>';
+
+}
+echo '</tbody></table>';
+// print_r ($champsInfo['data']["Aatrox"]["stats"]["hp"]);
+
+include 'footer.php';
+?>
